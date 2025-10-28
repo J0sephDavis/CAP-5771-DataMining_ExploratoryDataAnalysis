cf7bfe6e45308253fc191b4c666f40acea3f5ed5
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-28 09:59:51 -0400

    create a module for common graphs we have plotted.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
 	'context',
-	'compression'
+	'compression',
+	'plotting'
 ]
---
+++ helpers/plotting.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Tuple,List,Dict,Optional
+import matplotlib.ticker as ticker
+import numpy as np
+from pandas.plotting import scatter_matrix;
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import seaborn as sns
+import time
+
+DEFAULT_FIGURE_SIZE=(5,5)
+DEFAULT_FIGURE_DPI=300
+ROWS_TO_READ:Optional[int] = None
+
+def plot_joint_grid(dataset:pd.DataFrame, x_col:str,y_col:str, hue_column:str):
+	''' Plots a KDE joint grid plot between x & y.
+	- On the x and y axes, the KDE is done for each value.
+	- The center of the graph is a the 2d KDE plot.
+	'''
+	jg = sns.JointGrid(data=dataset,x=x_col,y=y_col)
+	jg.plot_joint(sns.kdeplot,bw_adjust=1,cut=1)
+	jg.plot_marginals(sns.kdeplot, data=dataset, hue=hue_column, legend=True, bw_adjust=0.7,cut=2)
+	legend = jg.ax_marg_x.get_legend()
+	leg_y = jg.ax_marg_y.get_legend()
+	if leg_y is not None:
+		leg_y.remove()
+	if legend is not None:
+		handles, labels = legend.legend_handles, [t.get_text() for t in legend.get_texts()]
+		if handles is not None:
+			legend.remove()
+			jg.ax_joint.legend(handles,labels, loc='upper right', frameon=True)
+	jg.set_axis_labels(xlabel=x_col,ylabel=y_col)
+	jg.figure.suptitle(t='{} by {} KDE'.format(x_col,y_col), y=0.995)
+	jg.figure.subplots_adjust(top=0.98, left=0.16)
+	return jg
+
+def perform_tsne(
+		tsne_data:pd.DataFrame, file_name_prefix:str,
+		perplexity:int,max_iter:int, no_progress_iter:int,
+	)->Tuple[pd.DataFrame, str,str]:
+	''' Performs TSNE & returns the results.
+	tsne_data: should be a copy of the data, containing all columns you want to process
+	Returns the tsne_results, the name of the x column, the name of the y column.
+	'''
+	time_started = time.time()
+	filename:str = f'{file_name_prefix} p:{perplexity} i:{max_iter}.tiff'
+	tsne_x_column:str = f'TSNE-X perp:{perplexity} max:{max_iter}'
+	tsne_y_column:str = f'TSNE-Y perp:{perplexity} max:{max_iter}'
+
+	tsne = TSNE(perplexity=perplexity, max_iter=max_iter, n_iter_without_progress=no_progress_iter)
+	tsne_res = tsne.fit_transform(tsne_data)
+	print('tsne P:{} completed in {} sec'.format(perplexity, time.time()-time_started))
+	
+	return (pd.DataFrame((tsne_res[:,1],tsne_res[:,1]), columns=[tsne_x_column, tsne_y_column]),
+		tsne_x_column, tsne_y_column
+	)
+
+def plot_tsne(
+		tsne_data:pd.DataFrame, x_column:str, y_column:str, file_name:str,
+		hue_column:Optional[str]=None, style_column:Optional[str] = None,
+		figure_size:Tuple[int,int]=DEFAULT_FIGURE_SIZE, figure_dpi:int=DEFAULT_FIGURE_DPI
+	):
+	''' Plots the TSNE data as a scatter plot. '''
+	f,ax = plt.subplots()
+	sns.scatterplot(ax=ax,
+		x=x_column, y=y_column,
+		hue=hue_column, style=style_column,
+		data=tsne_data, legend='auto',alpha=0.7,
+		palette=sns.color_palette("mako", as_cmap=True)
+	)
+	f.set_size_inches(figure_size)
+	f.set_dpi(figure_dpi)
+	f.savefig(file_name)
+	print(f'PLOT_TSNE, Saved figure: {file_name}')

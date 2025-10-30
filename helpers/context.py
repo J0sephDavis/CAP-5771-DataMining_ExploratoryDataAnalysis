68d3f9f9cf65e1c9bad321ccb665ef50a37ae12b
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 08:43:36 -0400

    I hate python. Why export my imports.... __all__ is time consuming to update for a class, just prefixed everything with _

--- helpers/context.py
+++ helpers/context.py
@@ -1,8 +1,20 @@
-from dotenv import load_dotenv, find_dotenv, dotenv_values
-from typing import Tuple, Dict, List, Optional, Any, Callable, Union
-from enum import StrEnum
-import datetime
-import os
+from dotenv import (
+	load_dotenv as _load_dotenv,
+	find_dotenv as _find_dotenv,
+	dotenv_values as _dotenv_values,
+)
+from typing import (
+	Tuple as _Tuple,
+	Dict as _Dict,
+	List as _List,
+	Optional as _Optional,
+	Any as _Any,
+	Callable as _Callable,
+	Union as _Union
+)
+from enum import StrEnum as _StrEnum
+import datetime as _datetime
+import os as _os
 
 class EnvException(Exception):
 	pass
@@ -12,12 +24,12 @@ class EnvExceptionMissingValue(EnvException):
 	pass
 
 def todays_date_iso8601()->str:
-	return datetime.datetime.now().strftime(r'%Y-%m-%d')
+	return _datetime.datetime.now().strftime(r'%Y-%m-%d')
 
 _default_dotenv_path:str = 'data.env'
 _unknown_field_prefix = '?!: '
 
-class EnvFields(StrEnum):
+class EnvFields(_StrEnum):
 	USER_LIST='MAL_USER_LIST'
 	RANKING_LIST='MAL_RANKING_LIST'
 	ANIME_LIST='MAL_ANIME_LIST'
@@ -30,14 +42,14 @@ class EnvFields(StrEnum):
 	ANIME_FILTERED='ANIME_FILTERED' # Where the filtered anime dataset can be found
 	ANIME_CLEANED='ANIME_CLEANED' # Where the cleaned dataset can be found
 
-_env_known_fields:List[str] = [x.value for x in EnvFields]
+_env_known_fields:_List[str] = [x.value for x in EnvFields]
 
 def _get_len_safe(val)->int:
 	""" Get val repr then measure len"""
 	val=str(val)
 	return len(val)
 
-def _get_max_lengths(env_vals:Dict, preprocess_value:Callable[[Any],Any])->Tuple[int,int]:
+def _get_max_lengths(env_vals:_Dict, preprocess_value:_Callable[[_Any],_Any])->_Tuple[int,int]:
 	''' Returns the maximum length of the keys & values from the passed dict. '''
 	max_key_len:int = 4
 	max_value_len:int = 6
@@ -49,16 +61,16 @@ def _get_max_lengths(env_vals:Dict, preprocess_value:Callable[[Any],Any])->Tuple
 		max_value_len = max(max_value_len, _get_len_safe(v)+1)
 	return max_key_len, max_value_len
 
-def pretty_print_key_val(env_vals:Dict, remove_prefix:Optional[str]=os.getcwd()):
+def pretty_print_key_val(env_vals:_Dict, remove_prefix:_Optional[str]=_os.getcwd()):
 	""" Pretty print a dictionary by key,val. Removes cwd from all values. """
 	if remove_prefix is not None:
-		def preprocess_value(value)->Any:
+		def preprocess_value(value)->_Any:
 			''' Removes prefix from string '''
 			if isinstance(value,str):
 				return value.removeprefix(remove_prefix)
 			return value
 	else:
-		def preprocess_value(value)->Any:
+		def preprocess_value(value)->_Any:
 			return value
 	max_key_len, max_value_len = _get_max_lengths(env_vals, preprocess_value)
 	
@@ -79,11 +91,11 @@ def pretty_print_key_val(env_vals:Dict, remove_prefix:Optional[str]=os.getcwd())
 def load_find_env(
 			dotenv_path:str=_default_dotenv_path, override_env:bool=True,
 			use_cwd:bool=True, raise_error_if_not_found:bool=True,
-			pretty_print_remove_suffix:Optional[str] = os.getcwd()
-		)->Tuple[bool,str]:
+			pretty_print_remove_suffix:_Optional[str] = _os.getcwd()
+		)->_Tuple[bool,str]:
 	""" Loads the dotenv & returns load_dotenv(), find_dotenv()"""
 	try:
-		path:str = find_dotenv(
+		path:str = _find_dotenv(
 			filename=dotenv_path, usecwd=use_cwd,
 			raise_error_if_not_found=raise_error_if_not_found
 		)
@@ -99,25 +111,25 @@ Please check the filename.
 {'-'*80}"""
 		) # End formatted block.
 		raise e # Raise again to stop execution & print traceback
-	success = load_dotenv(dotenv_path=path, override=override_env)
-	pretty_print_key_val(dotenv_values(path), f'{os.getcwd()}')
+	success = _load_dotenv(dotenv_path=path, override=override_env)
+	pretty_print_key_val(_dotenv_values(path), f'{_os.getcwd()}')
 	return success, path
 
-def get_env_val_safe(key:Union[StrEnum,str], default:Optional[str]=None)->str:
+def get_env_val_safe(key:_Union[_StrEnum,str], default:_Optional[str]=None)->str:
 	''' Get an env value safely
 	- If key is a StrEnum, we expect it to be in our known field list; raises EnvExceptionKeyError
 	- If value is None & default is None, raises EnvExceptionMissingValue
 	- Otherwise, returns value, or default if value is None
 	'''
-	if isinstance(key,StrEnum):
+	if isinstance(key,_StrEnum):
 		if key not in _env_known_fields:
 			raise EnvExceptionKeyError(f'Unknown field passed to get_env_val_safe')
 
-	val = os.getenv(key,default)
+	val = _os.getenv(key,default)
 		
 	if val is None:
 		raise EnvExceptionMissingValue(f'Could not get env value with key: s{key}')
 	return val
 
 if __name__ == '__main__':
-	loaded,path = load_find_env(pretty_print_remove_suffix=os.getcwd())
+	loaded,path = load_find_env(pretty_print_remove_suffix=_os.getcwd())

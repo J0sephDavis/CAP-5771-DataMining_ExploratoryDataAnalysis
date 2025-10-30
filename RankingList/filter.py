7c25449cdeea94b8b8fdeac8d53c59fa058dd393
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 10:59:35 -0400

    use  helper.files get_dataset

--- RankingList/filter.py
+++ RankingList/filter.py
@@ -1,9 +1,8 @@
 from helpers.context import (
-	load_find_env as _load_find_env,
 	get_env_val_safe as _get_env_val_safe,
 	EnvFields as _EnvFields,
 )
-from . import dataset as _dataset
+from .dataset import UserRankingColumn as _UserRankingColumn
 from pathlib import (
 	Path as _Path,
 )
@@ -12,20 +11,23 @@ from typing import (
 	Optional as _Optional,
 	List as _List,
 	Final as _Final,
+	Union as _Union,
 )
-from enum import (
-	StrEnum as _StrEnum,
-)
-
-def get_dataset(nrows:_Optional[int], use_cols:_Optional[_List[_dataset.UserRankingColumn]])->_pd.DataFrame:
+from helpers.files import get_dataset as _get_dataset
+def get_dataset(nrows:_Optional[int]=None, use_cols:_Optional[_List[_Union[_UserRankingColumn,str]]]=None)->_pd.DataFrame:
 	''' Reads the user ranking list from CSV & returns a dataframe. '''
 	ranking_file = _Path(_get_env_val_safe(_EnvFields.RANKING_FILTERED))
-	if not ranking_file.exists():
-		raise FileNotFoundError('Ranking File does not exist.')
-	return _pd.read_csv(
-		filepath_or_buffer=ranking_file,
+	return _get_dataset(
+		file=ranking_file,
 		nrows=nrows,
-		usecols=use_cols
+		use_cols=use_cols
+	)
+	
+def get_dataset_out(nrows:_Optional[int]=None, use_cols:_Optional[_List[_Union[_UserRankingColumn,str]]]=None)->_pd.DataFrame:
+	return _get_dataset(
+		file=_Path(_get_env_val_safe(_EnvFields.RANKING_FILTERED_OUT)),
+		nrows=nrows,
+		use_cols=use_cols
 	)
 
 def filter_dataset(raw_frame:_pd.DataFrame):

41851c56438780fe542e1cc16b0de607fdd83953
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 11:29:25 -0400

    filter dataset using the anime list dataset

--- RankingList/filter.py
+++ RankingList/filter.py
@@ -30,6 +30,21 @@ def get_dataset_out(nrows:_Optional[int]=None, use_cols:_Optional[_List[_Union[_
 		use_cols=use_cols
 	)
 
-def filter_dataset(raw_frame:_pd.DataFrame):
-	raise NotImplementedError()
-	return raw_frame
+from AnimeList.dataset import AnimeListColumns as _AnimeListColumns
+def filter_dataset(raw_rankings:_pd.DataFrame, anime_list:_pd.DataFrame):
+	frame = raw_rankings.copy()
+
+	# If the ANIME ID is in the anime_list, keep the record. Or maybe we make an entirely separate frame after filtering.
+	# If we do not perform this filter, we have a lot of data we consider semi-irrelevant. But that information is usefful in understanding a user?
+	rm_bad_anime_id = raw_rankings.loc[~(raw_rankings[_UserRankingColumn.ANIME_ID].isin(anime_list[_AnimeListColumns.ANIME_ID]))]
+
+	# Collate removed data & drop from frame.
+	removed = _pd.concat([
+		rm_bad_anime_id
+	])
+	frame.drop(index=removed.index, inplace=True)
+	RANKING_FILTERED = _Path(_get_env_val_safe(_EnvFields.RANKING_FOLDER_FILTER))
+	RANKING_FILTERED.mkdir(mode=0o775, parents=True,exist_ok=True)
+	frame.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED),index=False)
+	removed.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED_OUT),index=False)
+	return frame, removed

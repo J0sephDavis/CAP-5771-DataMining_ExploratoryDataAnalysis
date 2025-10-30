c137d133082433d553a756483cf5e61eb04f47b2
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 09:07:26 -0400

    define a strenum of the dataset columns

--- checkpoint2.py
+++ checkpoint2.py
@@ -16,3 +16,18 @@ if not loaded:
 5. [ ] Visualize covariant features
 6. [ ] UMAP, TSNE of data
 '''
+
+class UserRankingColumn(StrEnum):
+	''' Column names from the dataset. '''
+	USERNAME			='username'
+	ANIME_ID			='anime_id'				
+	WATCHED_EPISODES	='my_watched_episodes'
+	START_DATE			='my_start_date'		# Many 0000-00-00
+	FINISH_DATE			='my_finish_date'		# Many 0000-00-00
+	SCORE				='my_score'
+	STATUS				='my_status'
+	REWATCHING			='my_rewatching'		# Missing some values
+	REWATCHING_EP		='my_rewatching_ep'		# Many 0
+	LAST_UPDATED		='my_last_updated'
+	TAGS				='my_tags'				# Majority missing
+

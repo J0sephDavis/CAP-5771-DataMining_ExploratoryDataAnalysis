c22272f30bae19d5d734c0b912bdbd1c34ffa6b2
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 12:20:06 -0400

    call pre and post filter

--- checkpoint2.py
+++ checkpoint2.py
@@ -16,12 +16,13 @@ if not loaded:
 ''' TODO
 - [ ] Fetch dataset
 
-- [ ] Filter
-	- [ ] ANIME_ID must be in our filtered & cleaned ANIME dataset.
-	- [ ] 
 - [ ] Clean
 	- [ ] Remove invalid records
 	- [ ] Fix invalid values, if we need to.
+
+- [ ] Filter
+	- [ ] ANIME_ID must be in our filtered & cleaned ANIME dataset.
+	- [ ] 
 
 - [ ] Boxplots for each feature
 - [ ] Measure covariance of features
@@ -49,6 +50,7 @@ if not loaded:
 '''
 SUBSET_NROWS:Final[int] = int(dataset.raw_dataset_length)
 import time
+print('get user rankings')
 time_start = time.time()
 user_rankings = dataset.get_user_rankings(
 	nrows = SUBSET_NROWS, # load a fraction of the dataset
@@ -58,16 +60,54 @@ time_end = time.time()
 print(f'loaded user rankings in: {time_end-time_start} seconds')
 import AnimeList.clean
 from AnimeList.dataset import AnimeListColumns
+print('get anime list')
 anime_list:pd.DataFrame = AnimeList.clean.get_clean_dataset(None,[AnimeListColumns.ANIME_ID])
+
+# order of operations
+# - FILTER_1 (filters by anime_list)
+# - CLEAN_1 (removes invalid records)
+# - FILTER_2 (filters to what we want)
+
+# PRE FILTER - Remove records where the ANIME_ID is not found in our cleaned anime list
 try:
+	print('> Try load pre filter dataset from file')
+	pre_filtered = filter.get_dataset()
+	pre_filtered_out = filter.get_dataset_out()
+	print('Loaded pre filter dataset from disk.')
+except FileNotFoundError:
+	print('> pre Filter')
+	time_start = time.time()
+	pre_filtered, pre_filtered_out = filter.pre_filter_dataset(user_rankings,anime_list,False)
+	time_end = time.time()
+	print(f'PRE Filtered rankings in: {time_end-time_start} seconds')
+	print(f'PRE Filtering removed: {100.0*(pre_filtered_out.shape[0]/user_rankings.shape[0]):.2f}% ({pre_filtered_out.shape[0]}) of records')
+
+# CLEAN - Remove invalid/impossible values & replace values where we can.
+try:
+	print('> Try load clean dataset from file')
+	cleaned = clean.get_dataset()
+	cleaned_out = clean.get_dataset_out()
+	print('Loaded clean dataset from disk.')
+except FileNotFoundError:
+	print('> Clean')
+	time_start = time.time()
+	cleaned,cleaned_out = clean.clean_dataset(pre_filtered, anime_list, False)
+	time_end = time.time()
+	print(f'Cleaned rankings in: {time_end-time_start} seconds')
+	print(f'Cleaning removed: {100.0*(cleaned_out.shape[0]/pre_filtered.shape[0]):.2f}% ({cleaned.shape[0]}) of records')
+
+# POST FILTER - Get only the records we care about
+try:
+	print('> Try load pre filter dataset from file')
 	filtered = filter.get_dataset()
 	filtered_out = filter.get_dataset_out()
-	print('Loaded filter from disk.')
+	print('Loaded pre filter dataset from disk.')
 except FileNotFoundError:
+	print('> pre Filter')
 	time_start = time.time()
-	filtered, filtered_out = filter.filter_dataset(user_rankings,anime_list)
+	filtered, filtered_out = filter.pre_filter_dataset(user_rankings,anime_list,False)
 	time_end = time.time()
-	print(f'filtered rankings in: {time_end-time_start} seconds')
-	print(f'Filtering removed: {100.0*(filtered_out.shape[0]/user_rankings.shape[0]):.2f}% ({filtered_out.shape[0]}) of records')
+	print(f'PRE Filtered rankings in: {time_end-time_start} seconds')
+	print(f'PRE Filtering removed: {100.0*(filtered_out.shape[0]/cleaned.shape[0]):.2f}% ({filtered_out.shape[0]}) of records')
 
 exit()

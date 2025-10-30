bf9935d761646e99957071a1d9104381831d7cf6
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 12:19:55 -0400

    pre and post filter

--- RankingList/filter.py
+++ RankingList/filter.py
@@ -2,7 +2,10 @@ from helpers.context import (
 	get_env_val_safe as _get_env_val_safe,
 	EnvFields as _EnvFields,
 )
-from .dataset import UserRankingColumn as _UserRankingColumn
+from .dataset import (
+	UserRankingColumn as _UserRankingColumn,
+	StatusEnum as _StatusEnum,
+)
 from pathlib import (
 	Path as _Path,
 )
@@ -30,20 +33,48 @@ def get_dataset_out(nrows:_Optional[int]=None, use_cols:_Optional[_List[_Union[_
 	)
 
 from AnimeList.dataset import AnimeListColumns as _AnimeListColumns
-def filter_dataset(raw_rankings:_pd.DataFrame, anime_list:_pd.DataFrame):
+
+
+
+
+def pre_filter_dataset(raw_rankings:_pd.DataFrame, anime_list:_pd.DataFrame, save_to_csv:bool):
+	''' Just filters based on the anime_list dataset to remove irrelevant records.'''
 	frame = raw_rankings.copy()
 
 	# If the ANIME ID is in the anime_list, keep the record. Or maybe we make an entirely separate frame after filtering.
 	# If we do not perform this filter, we have a lot of data we consider semi-irrelevant. But that information is usefful in understanding a user?
 	rm_bad_anime_id = raw_rankings.loc[~(raw_rankings[_UserRankingColumn.ANIME_ID].isin(anime_list[_AnimeListColumns.ANIME_ID]))]
+	
+	frame.drop(index=rm_bad_anime_id.index, inplace=True)
+	if save_to_csv:
+		_Path(_get_env_val_safe(_EnvFields.RANKING_FOLDER_FILTER)).mkdir(
+			mode=0o775, parents=True,exist_ok=True
+		)
+		frame.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED_PRE),index=False)
+		rm_bad_anime_id.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED_PRE_OUT),index=False)
+	return frame, rm_bad_anime_id
 
+def filter_dataset(cleaned_rankings:_pd.DataFrame, anime_list:_pd.DataFrame, save_to_csv:bool):
+	''' Filters based on criteria. Aka the Post Filter. '''
+	frame = cleaned_rankings.copy()
+	'''TODO
+	- [ ] Status
+		- Remove PTW
+		- Remove Currently watching & watched < 0.50?
+		- Removed dropped where they did not watch until a threashold?
+	- [ ] WATCHED_EPISODES
+		- Watched Episodes = 0, drop
+	'''
+	rm_plan_to_watch = frame.loc[frame[_UserRankingColumn.STATUS]==_StatusEnum.PLAN_TO_WATCH]
 	# Collate removed data & drop from frame.
 	removed = _pd.concat([
-		rm_bad_anime_id
+		rm_plan_to_watch,
 	])
 	frame.drop(index=removed.index, inplace=True)
-	RANKING_FILTERED = _Path(_get_env_val_safe(_EnvFields.RANKING_FOLDER_FILTER))
-	RANKING_FILTERED.mkdir(mode=0o775, parents=True,exist_ok=True)
-	frame.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED),index=False)
-	removed.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED_OUT),index=False)
+	if save_to_csv:
+		_Path(_get_env_val_safe(_EnvFields.RANKING_FOLDER_FILTER)).mkdir(
+			mode=0o775, parents=True,exist_ok=True
+		)
+		frame.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED),index=False)
+		removed.to_csv(_get_env_val_safe(_EnvFields.RANKING_FILTERED_OUT),index=False)
 	return frame, removed

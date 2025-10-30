9958dbeabd87ba9cb03866e625d7ae8acf5db1b6
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 08:47:41 -0400

    TODO

---
+++ checkpoint2.py
@@ -0,0 +1,14 @@
+from helpers.context import load_find_env, get_env_val_safe
+loaded, _ = load_find_env()
+if not loaded:
+	print('DOTENV could not be loaded. STOP.')
+	exit()
+
+''' TODO
+1. [ ] Fetch dataset
+2. [ ] Filter and Clean
+3. [ ] Boxplots for each feature
+4. [ ] Measure covariance of features
+5. [ ] Visualize covariant features
+6. [ ] UMAP, TSNE of data
+'''

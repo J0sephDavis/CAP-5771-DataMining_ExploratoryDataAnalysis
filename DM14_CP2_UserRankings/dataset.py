bf30b28f06232e9c4e2b6664fa554de338bb9819
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 09:19:13 -0400

    Move code to its own module

---
+++ DM14_CP2_UserRankings/dataset.py
@@ -0,0 +1,48 @@
+from helpers.context import (
+	load_find_env as _load_find_env,
+	get_env_val_safe as _get_env_val_safe,
+	EnvFields as _EnvFields
+)
+from pathlib import (
+	Path as _Path
+)
+import pandas as _pd
+from typing import (
+	Optional as _Optional,
+	List as _List,
+	Final as _Final
+)
+from enum import (
+	StrEnum as _StrEnum
+)
+
+class UserRankingColumn(_StrEnum):
+	''' Column names from the dataset. '''
+	USERNAME			='username'
+	ANIME_ID			='anime_id'				
+	WATCHED_EPISODES	='my_watched_episodes'
+	START_DATE			='my_start_date'		# Many 0000-00-00
+	FINISH_DATE			='my_finish_date'		# Many 0000-00-00
+	SCORE				='my_score'
+	STATUS				='my_status'
+	REWATCHING			='my_rewatching'		# Missing some values
+	REWATCHING_EP		='my_rewatching_ep'		# Many 0
+	LAST_UPDATED		='my_last_updated'
+	TAGS				='my_tags'				# Majority missing
+
+raw_dataset_length:_Final[int] = 80076112
+columns_for_retrieval:_Final[_List[UserRankingColumn]] = [
+	UserRankingColumn.ANIME_ID,
+	UserRankingColumn.USERNAME,
+	UserRankingColumn.WATCHED_EPISODES,
+	UserRankingColumn.SCORE,
+	UserRankingColumn.STATUS
+]
+def get_user_rankings(nrows:_Optional[int], use_cols:_Optional[_List[UserRankingColumn]])->_pd.DataFrame:
+	''' Reads the user ranking list from CSV & returns a dataframe. '''
+	user_ranking_file = _Path(_get_env_val_safe(_EnvFields.RANKING_LIST))
+	return _pd.read_csv(
+		filepath_or_buffer=user_ranking_file,
+		nrows=nrows,
+		usecols=use_cols
+	)
--- checkpoint2.py
+++ checkpoint2.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 from typing import Optional, List, Final
 from enum import StrEnum
+import DM14_CP2_UserRankings.dataset as dataset
 loaded, _ = load_find_env()
 if not loaded:
 	print('DOTENV could not be loaded. STOP.')
@@ -17,33 +18,6 @@ if not loaded:
 6. [ ] UMAP, TSNE of data
 '''
 
-class UserRankingColumn(StrEnum):
-	''' Column names from the dataset. '''
-	USERNAME			='username'
-	ANIME_ID			='anime_id'				
-	WATCHED_EPISODES	='my_watched_episodes'
-	START_DATE			='my_start_date'		# Many 0000-00-00
-	FINISH_DATE			='my_finish_date'		# Many 0000-00-00
-	SCORE				='my_score'
-	STATUS				='my_status'
-	REWATCHING			='my_rewatching'		# Missing some values
-	REWATCHING_EP		='my_rewatching_ep'		# Many 0
-	LAST_UPDATED		='my_last_updated'
-	TAGS				='my_tags'				# Majority missing
-
-def get_user_rankings(nrows:Optional[int], use_cols:Optional[List[UserRankingColumn]])->pd.DataFrame:
-	''' Reads the user ranking list from CSV & returns a dataframe. '''
-	user_ranking_file:Path = Path(get_env_val_safe(EnvFields.RANKING_LIST))
-	return pd.read_csv(
-		filepath_or_buffer=user_ranking_file,
-		nrows=nrows,
-		usecols=use_cols
-	)
-raw_dataset_length:Final[int] = 80076112
-user_rankings = get_user_rankings(
-	nrows = int(raw_dataset_length*0.15), # load a fraction of the dataset
-	use_cols = None
-)
 ''' NOTE
  	My computer likely cannot handle loading this entire dataset at once.
 	 	There are 80,076,112 records.
@@ -62,4 +36,8 @@ user_rankings = get_user_rankings(
 		- SCORE
 		- STATUS
 '''
+user_rankings = dataset.get_user_rankings(
+	nrows = int(dataset.raw_dataset_length*0.15), # load a fraction of the dataset
+	use_cols = dataset.columns_for_retrieval
+)
 exit()

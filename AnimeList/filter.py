aa3701b918a02635a38b934da8119b9f2a8e102f
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-10-30 10:40:30 -0400

    update filter

--- AnimeList/filter.py
+++ AnimeList/filter.py
@@ -10,18 +10,48 @@ import pandas as _pd
 from typing import (
 	Optional as _Optional,
 	List as _List,
-	Sequence as _Sequence,
-	Final as _Final,
 	Union as _Union,
+	Tuple as _Tuple,
 )
-from enum import (
-	StrEnum as _StrEnum
-)
-from . import dataset as _dataset
+from .dataset import AnimeListColumns as _AnimeListColumns
+
 def get_dataset(
 		nrows:_Optional[int] = None,
-		use_cols:_Optional[_List[_Union[_dataset.AnimeListColumns,str]]] = None
+		use_cols:_Optional[_List[_Union[_AnimeListColumns,str]]] = None
 	)->_pd.DataFrame:
-	''' Load the AnimeList.csv dataset into a dataframe. '''
+	''' Load the filtered dataset. '''
 	PATH_TO_DATASET = _Path(_get_env_val_safe(_EnvFields.ANIME_FILTERED))
 	return _files.get_dataset(PATH_TO_DATASET, nrows=nrows, use_cols=use_cols)
+
+def get_out_dataset(
+		nrows:_Optional[int] = None,
+		use_cols:_Optional[_List[_Union[_AnimeListColumns,str]]] = None
+	)->_pd.DataFrame:
+	''' Load the records that were REMOVED from the dataset. '''
+	PATH_TO_DATASET = _Path(_get_env_val_safe(_EnvFields.ANIME_FILTERED_OUT))
+	return _files.get_dataset(PATH_TO_DATASET, nrows=nrows, use_cols=use_cols)
+
+
+def filter_dataset(anime_list:_pd.DataFrame)->_Tuple[_pd.DataFrame, _pd.DataFrame]:
+	'''
+	Apply filtering rules to the AnimeList dataset.
+	Returns the filtered frame & another frame containing the dropped records.
+	'''
+	frame = anime_list.copy()
+	# Records
+	removed_records = frame.loc[ # for analysis
+		(frame[_AnimeListColumns.STATUS] != 'Finished Airing')
+		| ((frame[_AnimeListColumns.TYPE]=='Music') | (frame[_AnimeListColumns.TYPE]=='Unknown'))
+	].copy()
+
+	frame.drop(index=removed_records.index, inplace=True)
+	# ^^^ If this doesn't work, just uncomment below and delete it. Not testing today.
+	# frame.drop( # Remove all results which have not finished airing
+	# 	index=frame[frame[_AnimeListColumns.STATUS]!='Finished Airing'].index,
+	# 	inplace=True
+	# )
+	# frame.drop( # Drop all results which are of type 'Music' or 'Unknown'.
+	# 	index=frame[(frame[_AnimeListColumns.TYPE]=='Music')|(frame[_AnimeListColumns.TYPE]=='Unknown')].index,
+	# 	inplace=True
+	# )
+	return frame, removed_records

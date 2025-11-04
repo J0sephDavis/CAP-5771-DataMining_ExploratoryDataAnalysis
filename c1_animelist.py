3f30834b84bc549aa019571b186c4a73d6744775
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-04 10:01:11 -0500

    use datasetdescriptor enum

--- c1_animelist.py
+++ c1_animelist.py
@@ -17,6 +17,9 @@ import pandas as pd
 from AnimeList.plotting import (
     compare_by_label, comparison_barchart_by_type
 )
+from AnimeList import (
+    DatasetDescriptors
+)
 from pathlib import Path
 from helpers.context import (
     EnvFields,
@@ -37,7 +40,7 @@ for key in folders:
 dataset = AnimeListRaw()
 filtered, filter_removed = filter_dataset(dataset)
 clean, clean_removed = clean_dataset(filtered)
-column_label:Final[str] = 'COMARE_LABEL'
+column_label:Final[str] = DatasetDescriptors.ColumnName
 def __label_copy(db:DatasetBase, label:str)->pd.DataFrame:
     frame = db.get_frame().copy()
     frame[column_label] = label
@@ -60,13 +63,27 @@ def plot_removed(
     F01.savefig(f'{folder}/F01.tiff')
     return
 
+def plot_comparsion(raw:AnimeListRaw, filtered:AnimeListFiltered, clean:AnimeListClean):
+    ''' Compare all of the datasets. '''
+    R = __label_copy(raw,DatasetDescriptors.Raw)
+    F = __label_copy(filtered,DatasetDescriptors.Filtered)
+    C = __label_copy(clean, DatasetDescriptors.Cleaned)
+    comp_frame = pd.concat([R,F,C],axis=0)
+    compare_by_label(
+        comp_frame,
+        column_label,
+        get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_COMPARISON)
+    )
+    return
+
 plot_removed(
     result=filtered,removed=filter_removed,
-    result_label='Filtered', removed_label='Removed',
+    result_label=DatasetDescriptors.Filtered, removed_label='Removed',
     folder=Path(get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_FILTER))
 )
 plot_removed(
     result=filtered,removed=filter_removed,
-    result_label='Cleaned', removed_label='Removed',
+    result_label=DatasetDescriptors.Cleaned, removed_label='Removed',
     folder=Path(get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_CLEAN))
 )
+plot_comparsion(dataset,filtered,clean)

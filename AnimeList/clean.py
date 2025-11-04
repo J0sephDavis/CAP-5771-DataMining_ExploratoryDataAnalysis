938655001167d5e587063ac1f6e79dd717efb19c
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-04 09:07:34 -0500

    use new methods in clean file

--- AnimeList/clean.py
+++ AnimeList/clean.py
@@ -2,7 +2,7 @@ from helpers.context import (
 	get_env_val_safe as _get_env_val_safe,
 	EnvFields as _EnvFields,
 )
-from helpers import files as _files
+from helpers.files import DatasetBase as _DatasetBase
 from pathlib import (
 	Path as _Path,
 )
@@ -18,28 +18,37 @@ from typing import (
 from enum import (
 	StrEnum as _StrEnum
 )
-from . import dataset as _dataset
+from .filter import AnimeListFiltered as _AnimeListFiltered
 
-def get_clean_dataset(
-		nrows:_Optional[int] = None,
-		use_cols:_Optional[_List[_Union[_dataset.AnimeListColumns,str]]] = None
-	)->_pd.DataFrame:
-	''' Load the clean dataset. '''
-	PATH_TO_DATASET = _Path(_get_env_val_safe(_EnvFields.ANIME_CLEANED))
-	return _files.get_dataset(PATH_TO_DATASET, nrows=nrows, use_cols=use_cols)
+class AnimeListClean(_DatasetBase):
+	''' The AnimeList that has been cleaned. '''
+	def __init__(self,
+		frame:_Optional[_pd.DataFrame] = None,
+		nrows:_Optional[int] = None
+			)->None:
+		super().__init__(
+			path=_Path(_get_env_val_safe(_EnvFields.ANIME_CLEANED)),
+			frame=frame,
+			nrows=nrows,
+		)
 
-def get_out_dataset(
-		nrows:_Optional[int] = None,
-		use_cols:_Optional[_List[_Union[_dataset.AnimeListColumns,str]]] = None
-	)->_pd.DataFrame:
-	''' Load the records that were REMOVED from the dataset during cleaning. '''
-	PATH_TO_DATASET = _Path(_get_env_val_safe(_EnvFields.ANIME_CLEANED_OUT))
-	return _files.get_dataset(PATH_TO_DATASET, nrows=nrows, use_cols=use_cols)
-
-
-def clean_dataset(anime_list:_pd.DataFrame)->_Tuple[_pd.DataFrame, _pd.DataFrame]:
+class AnimeListCleanOut(_DatasetBase):
+	''' The AnimeList records that have been cleaned out. '''
+	def __init__(self,
+		frame:_Optional[_pd.DataFrame] = None,
+		nrows:_Optional[int] = None
+			)->None:
+		super().__init__(
+			path=_Path(_get_env_val_safe(_EnvFields.ANIME_CLEANED_OUT)),
+			frame=frame,
+			nrows=nrows,
+		)
+def clean_dataset(anime_list:_AnimeListFiltered)->_Tuple[AnimeListClean, AnimeListCleanOut]:
 	''' Apply cleaning rules to the AnimeList dataset. Return the clean set & the records that were removed. '''
-	frame = anime_list.copy()
+	frame = anime_list.get_frame()
+	if frame is None:
+		raise Exception('')
+	frame = frame.copy()
 	impossible_score:_pd.Series[bool] = (frame['score']<1)|(frame['score']>10)|(frame['score'].isnull())
 	no_members:_pd.Series[bool] = (frame['members']==0)|(frame['members'].isnull())
 	invalid_status:_pd.Series[bool] = (frame['status'].isnull())
@@ -47,6 +56,4 @@ def clean_dataset(anime_list:_pd.DataFrame)->_Tuple[_pd.DataFrame, _pd.DataFrame
 	REMOVE = (impossible_score | no_members | invalid_status | no_genre)
 	removed = frame.loc[REMOVE].copy()
 	frame.drop(index=removed.index,inplace=True)
-	frame.to_csv(_get_env_val_safe(_EnvFields.ANIME_CLEANED))
-	removed.to_csv(_get_env_val_safe(_EnvFields.ANIME_CLEANED_OUT))
-	return frame, removed
+	return AnimeListClean(frame=frame), AnimeListCleanOut(frame=removed)

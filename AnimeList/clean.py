8766efa72b0fdc3db88a0a2dcfe9ef1e6bd5cec3
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-09 13:43:12 -0500

    set usecols in AnimeListClean

--- AnimeList/clean.py
+++ AnimeList/clean.py
@@ -11,8 +11,11 @@ import pandas as _pd
 from typing import (
 	Optional as _Optional,
 	Tuple as _Tuple,
+	List as _List,
+	Union as _Union,
 )
 from .filter import AnimeListFiltered as _AnimeListFiltered
+from .dataset import AnimeListColumns as _AnimeListColumns
 import logging as _logging
 from helpers.context import APP_LOGGER_NAME as _APP_LOGGER_NAME
 _logger = _logging.getLogger(f'{_APP_LOGGER_NAME}.AnimeList.clean')
@@ -20,13 +23,14 @@ class AnimeListClean(_DatasetCSV):
 	''' The AnimeList that has been cleaned. '''
 	def __init__(self,
 			  	frame:_Optional[_pd.DataFrame]=None,
-			  	path:_Path = _Path(_get_env_val_safe(_EnvFields.CSV_ANIME_CLEAN))
+			  	path:_Path = _Path(_get_env_val_safe(_EnvFields.CSV_ANIME_CLEAN)),
+				usecols:_Optional[_List[_Union[str,_AnimeListColumns]]] = None
 			  ) -> None:
 		super().__init__(frame, path)
 		_logger.debug(f'AnimeListClean.__init__(frame:{frame},path:{path})')
 		if self.frame is None:
 			_logger.debug('attempting to load csv')
-			self.load()
+			self.load(usecols=usecols)
 
 class AnimeListCleanOut(_DatasetCSV):
 	''' The AnimeList records that have been cleaned out. '''

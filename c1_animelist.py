5d7c3ba841b9c1505af08a6c00c0d98369095726
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-09 10:03:58 -0500

    restructure checkpoint 1 into a single method()

--- c1_animelist.py
+++ c1_animelist.py
@@ -1,89 +1,95 @@
-from helpers.files import (
-    DatasetBase
-)
+from helpers.files import DatasetBase
 from AnimeList.filter import (
     AnimeListFiltered, AnimeListFilterOut, filter_dataset
 )
 from AnimeList.clean import (
     AnimeListClean, AnimeListCleanOut, clean_dataset
 )
-from AnimeList.dataset import (
-    AnimeListRaw
-)
-from typing import (
-    Tuple, Final, List
-)
+from AnimeList.dataset import AnimeListRaw
+from typing import List
 import pandas as pd
 from AnimeList.plotting import (
     compare_by_label, comparison_barchart_by_type
 )
-from AnimeList import (
-    DatasetDescriptors
-)
+from AnimeList import DatasetDescriptors
 from pathlib import Path
 from helpers.context import (
     EnvFields,
     get_env_val_safe,
-    load_find_env
 )
-load_find_env()
-folders:List[EnvFields] = [
-    EnvFields.DIR_FIGURES_ANIME,
-    EnvFields.DIR_FIGURES_ANIME_CLEAN,
-    EnvFields.DIR_FIGURES_ANIME_COMPARISON,
-    EnvFields.DIR_FIGURES_ANIME_FILTER,
-    EnvFields.DIR_GENERATED_DATASETS,
-]
-for key in folders:
-    Path(get_env_val_safe(key)).mkdir(exist_ok=True, parents=True, mode=0o777)
-
-dataset = AnimeListRaw()
-filtered, filter_removed = filter_dataset(dataset)
-clean, clean_removed = clean_dataset(filtered)
-column_label:Final[str] = DatasetDescriptors.ColumnName
-def __label_copy(db:DatasetBase, label:str)->pd.DataFrame:
+def __label_copy(db:DatasetBase, label:str, descriptor_col:str)->pd.DataFrame:
     frame = db.get_frame().copy()
-    frame[column_label] = label
+    frame[descriptor_col] = label
     return frame
 
 def plot_removed(
-        result:DatasetBase, removed:DatasetBase,
-        result_label:str, removed_label:str,
-        folder:Path
+        result:DatasetBase,
+        removed:DatasetBase,
+        result_label:str,
+        removed_label:str,
+        folder:Path,
+        descriptor_col:str
         )->None:
     ''' Compares the two datasets by creating graphs '''
-    res = __label_copy(result,result_label)
-    rem = __label_copy(removed,removed_label)
+    res = __label_copy(result,result_label, descriptor_col)
+    rem = __label_copy(removed,removed_label, descriptor_col)
     compFrame = pd.concat([res,rem],axis=0)
     if not folder.exists():
         folder.mkdir(mode=0o777, parents=True, exist_ok=True)
-    compare_by_label(compFrame, column_label, str(folder))
+    compare_by_label(compFrame, descriptor_col, str(folder))
     F01, F01_ax = comparison_barchart_by_type(res, rem)
     F01.suptitle('Clean Results')
     F01.savefig(f'{folder}/F01.tiff')
     return
 
-def plot_comparsion(raw:AnimeListRaw, filtered:AnimeListFiltered, clean:AnimeListClean):
+def plot_comparsion(
+            raw:AnimeListRaw,
+            filtered:AnimeListFiltered,
+            clean:AnimeListClean,
+            descriptor_col:str
+        )->None:
     ''' Compare all of the datasets. '''
-    R = __label_copy(raw,DatasetDescriptors.Raw)
-    F = __label_copy(filtered,DatasetDescriptors.Filtered)
-    C = __label_copy(clean, DatasetDescriptors.Cleaned)
+    R = __label_copy(raw,DatasetDescriptors.Raw, descriptor_col)
+    F = __label_copy(filtered,DatasetDescriptors.Filtered, descriptor_col)
+    C = __label_copy(clean, DatasetDescriptors.Cleaned, descriptor_col)
     comp_frame = pd.concat([R,F,C],axis=0)
     compare_by_label(
         comp_frame,
-        column_label,
+        descriptor_col,
         get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_COMPARISON)
     )
     return
 
-plot_removed(
-    result=filtered,removed=filter_removed,
-    result_label=DatasetDescriptors.Filtered, removed_label='Removed',
-    folder=Path(get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_FILTER))
-)
-plot_removed(
-    result=filtered,removed=filter_removed,
-    result_label=DatasetDescriptors.Cleaned, removed_label='Removed',
-    folder=Path(get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_CLEAN))
-)
-plot_comparsion(dataset,filtered,clean)
+def run():
+    folders:List[EnvFields] = [
+        EnvFields.DIR_FIGURES_ANIME,
+        EnvFields.DIR_FIGURES_ANIME_CLEAN,
+        EnvFields.DIR_FIGURES_ANIME_COMPARISON,
+        EnvFields.DIR_FIGURES_ANIME_FILTER,
+        EnvFields.DIR_GENERATED_DATASETS,
+    ]
+    for key in folders:
+        Path(get_env_val_safe(key)).mkdir(exist_ok=True, parents=True, mode=0o777)
+
+    dataset = AnimeListRaw()
+    filtered, filter_removed = filter_dataset(dataset)
+    clean, clean_removed = clean_dataset(filtered)
+    descriptor_col:str = DatasetDescriptors.ColumnName
+    plot_removed(
+        result=filtered,removed=filter_removed,
+        result_label=DatasetDescriptors.Filtered, removed_label='Removed',
+        folder=Path(get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_FILTER)),
+        descriptor_col=descriptor_col
+    )
+    plot_removed(
+        result=filtered,removed=filter_removed,
+        result_label=DatasetDescriptors.Cleaned, removed_label='Removed',
+        folder=Path(get_env_val_safe(EnvFields.DIR_FIGURES_ANIME_CLEAN)),
+        descriptor_col=descriptor_col
+    )
+    plot_comparsion(
+        raw=dataset,
+        filtered=filtered,
+        clean=clean,
+        descriptor_col=descriptor_col
+    )

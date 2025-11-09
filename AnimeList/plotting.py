76681ddc563dfdef4817c69ba72cd0b6382f0b66
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-09 10:21:03 -0500

    logging

--- AnimeList/plotting.py
+++ AnimeList/plotting.py
@@ -1,32 +1,45 @@
-from helpers.plotting import DEFAULT_FIGURE_DPI, DEFAULT_FIGURE_SIZE
-import pandas as pd
-from typing import Tuple, List
-import matplotlib.pyplot as plt
+from dataset.plotting import DEFAULT_FIGURE_DPI, DEFAULT_FIGURE_SIZE
+import pandas as _pd
+from typing import (
+	Tuple as _Tuple,
+	List as _List,
+	Optional as _Optional,
+)
+import matplotlib.pyplot as _plt
+from matplotlib.figure import Figure as _Figure
+from matplotlib.axes import Axes as _Axes
+import logging as _logging
+from helpers.context import APP_LOGGER_NAME as _APP_LOGGER_NAME
+_logger = _logging.getLogger(f'{_APP_LOGGER_NAME}.AnimeList.plotting')
 # # Plotting Functions
-def barchart_by_type(frame:pd.DataFrame,
+def barchart_by_type(frame:_pd.DataFrame,
 		figsize=DEFAULT_FIGURE_SIZE, figure_dpi=DEFAULT_FIGURE_DPI
-	)->Tuple[plt.Figure, plt.Axes]:
+	)->_Tuple[_Figure, _Axes]:
 	""" Plots the amount of entries by the categorical feature 'type'. Returns figure, axes """
+	_logger.debug('barchart_by_type')
 	type_counts = frame['type'].value_counts()
-	figure,axes = plt.subplots(dpi=figure_dpi)
+	figure,axes = _plt.subplots(dpi=figure_dpi)
 	type_counts.plot.bar(ax=axes, figsize=figsize)
 	axes.set_xlabel('Content Types')
 	axes.set_ylabel('Count')
 	axes.set_title('Entries by Content Type')
 	return figure,axes
 
-def boxplot_by_type(frame:pd.DataFrame,
-		column:str, exclude_types:List[str]=[], sub_by:str|None = None,
+def boxplot_by_type(frame:_pd.DataFrame,
+		column:str, exclude_types:_List[str]=[], sub_by:_Optional[str] = None,
 		plot_overall:bool=False, plot_aggregate:bool=False, plot_all_except_include:bool=False,
 		show_fliers:bool=True, show_mean:bool=True,
 		figure_dpi:int = 300,
 		**matplot_kwargs
-	)->Tuple[plt.Figure,plt.Axes]:
+	)->_Tuple[_Figure,_Axes]:
 	""" Plots the column by type. plot_overall shows the entire dataframes boxplot. plot_aggregate respects exclude_types """
+	_logger.debug('boxplot_by_type')
 	if not (column in frame.columns):
-		raise KeyError('boxplot_by_type {} not found in frame.columns'.format(column))
+		err = KeyError('boxplot_by_type {} not found in frame.columns'.format(column))
+		_logger.error('boxplot_by_type:', exc_info=err)
+		raise err
 	
-	figure,axes = plt.subplots(dpi=figure_dpi)
+	figure,axes = _plt.subplots(dpi=figure_dpi)
 	# Set the default figure size to 20in by 20in...
 	# Set default width to 0.5 * width of graph (must be set to get them to auto adjust when plotting overall/aggregate)
 	matplot_kwargs.setdefault('figsize',DEFAULT_FIGURE_SIZE)
@@ -42,7 +55,7 @@ def boxplot_by_type(frame:pd.DataFrame,
 		)
 
 	labels = [label.get_text() for label in axes.get_xticklabels()]
-	def additional_plot(df:pd.DataFrame, label:str):
+	def additional_plot(df:_pd.DataFrame, label:str):
 		df.boxplot(
 			column=[column], ax=axes, by = (sub_by if sub_by is not None else None),
 			showfliers=show_fliers,
@@ -77,32 +90,31 @@ def boxplot_by_type(frame:pd.DataFrame,
 	return figure,axes
 
 def comparison_barchart_by_type(
-		filtered_frame:pd.DataFrame,
-		removed_frame:pd.DataFrame,
+		filtered_frame:_pd.DataFrame,
+		removed_frame:_pd.DataFrame,
 		figure_dpi=DEFAULT_FIGURE_DPI,
 		**matplot_kwargs
-	)->Tuple[plt.Figure, plt.Axes]:
+	)->_Tuple[_Figure, _Axes]:
 	""" Stacked barchart showing removed vs remaining records """
+	_logger.info('comparison_barchart_by_type')
 	matplot_kwargs.setdefault('figsize',DEFAULT_FIGURE_SIZE)
 	leftover = filtered_frame['type'].value_counts()
-	print('leftover:')
-	print(leftover)
+	_logger.info(f'leftover:\n{leftover}')
 	leftover.name='Leftover'
 
 	removed = removed_frame['type'].value_counts()
 	removed.name='Removed'
-	print('removed:')
-	print(removed)
+	_logger.info(f'removed:\n{removed}')
 	total_records = (leftover.sum() + removed.sum())
 	total_left = leftover.sum()
 	total_removed = removed.sum()
 	float_removed = total_removed / total_records
-	print('total records: {}'.format(total_records))
-	print('total_left: {}'.format(total_left))
-	print('total_removed: {}'.format(total_removed))
-	print('float_removed: {}'.format(float_removed))
-	figure,axes = plt.subplots(dpi=figure_dpi)
-	tdf = pd.concat([leftover,removed],axis=1)
+	_logger.info('total records: {}'.format(total_records))
+	_logger.info('total_left: {}'.format(total_left))
+	_logger.info('total_removed: {}'.format(total_removed))
+	_logger.info('float_removed: {}'.format(float_removed))
+	figure,axes = _plt.subplots(dpi=figure_dpi)
+	tdf = _pd.concat([leftover,removed],axis=1)
 	tdf.plot(ax=axes, kind='bar',stacked=True, rot=0, **matplot_kwargs)
 	axes.set_xlabel('')
 	# axes.set_ylabel('Count')
@@ -110,7 +122,7 @@ def comparison_barchart_by_type(
 
 # ------------------------------------------------------------------------------
 def compare_by_label(
-		comparison_frame:pd.DataFrame,
+		comparison_frame:_pd.DataFrame,
 		column_label:str,
 		figure_folder:str
 	)->None:
@@ -118,6 +130,7 @@ def compare_by_label(
 	- generate multiple boxplots describing the distribution shift
 		with respect to several features.
 	'''
+	_logger.debug('compare_by_label')
 	rotation=12
 	# Compare each type before & after filtering. Score Distributions
 	F02A, F02A_ax = boxplot_by_type(

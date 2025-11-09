163afe383a4442d0bc5ac38dbbf6b27bf17eff8a
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-09 09:04:36 -0500

    new exception type

--- helpers/exceptions.py
+++ helpers/exceptions.py
@@ -2,6 +2,10 @@ class DatasetExceptionBase(Exception):
     ''' The base class for all dataset exceptions'''
     pass
 
+class DatasetNotFound(DatasetExceptionBase, FileNotFoundError):
+    ''' The file was not found. '''
+    pass
+
 class DatasetFileExists(DatasetExceptionBase, FileExistsError):
     ''' The file you want to save already exists. '''
     pass

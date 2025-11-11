bbb5a50a70f65a64036f189054a694e3350e314f
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-11 13:14:45 -0500

    kk

--- checkpoint2.py
+++ checkpoint2.py
@@ -212,7 +212,8 @@ def run():
 		n=testdata.get_shape()[1]
 		sim_total=0.0
 		rat_sim_total=0.0
-		max_k=min(*testdata.get_shape())-1 # we start with the maximum k so we can calculate the optimal k
+		m = testdata.get_shape()[0]
+		max_k=min(*testdata.get_shape(),257)-1 # Cannot allocate 532 GiB lol
 		_logger.debug(f'max_k = {max_k}')
 		u,sigma,vt=linalg.svds(testdata,k=max_k)
 		

9839c436b7f46a53b84f8f0542c7a8ba89696f07
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-26 15:20:42 -0500

    indentation

--- checkpoint_final.py
+++ checkpoint_final.py
@@ -1,10 +1,10 @@
 '''
 GOAL: get two pages worth of content?
 - homophily measure?
-    - Directional/Asymmetric
-        - Intersection A&B / count(A)?
-        - For sum of x in A & B x_a.rating-x_b.rating
-            - average difference in ratings?
+	- Directional/Asymmetric
+		- Intersection A&B / count(A)?
+		- For sum of x in A & B x_a.rating-x_b.rating
+			- average difference in ratings?
 '''
 from helpers.context import (
 	get_env_val_safe,
@@ -21,13 +21,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 def main():
-    ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
-    # Create Users-Content with score as value
-    ucs=UserContentScore(ulf)
-    del ulf
-    # Perform UMAP
-    umap_results = ucs.run_umap()
-    f,ax = plt.subplots()
+	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
+	# Create Users-Content with score as value
+	ucs=UserContentScore(ulf)
+	del ulf
+	# Perform UMAP
+	umap_results = ucs.run_umap()
+	f,ax = plt.subplots()
 	sns.scatterplot(ax=ax,
 		x=0, y=1,
 		hue=umap_results[1]
@@ -37,4 +37,4 @@ def main():
 	f.set_size_inches(10,10)
 	f.set_dpi(500)
 	f.savefig('usercontent_umap_noargs.tiff')
-    # Attempt to make a user-user similarity measure by finding users with similar ratings.
+	# Attempt to make a user-user similarity measure by finding users with similar ratings.

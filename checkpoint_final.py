79836a9482fb541a0000b86f38c5b3a7e103c6d9
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-26 15:19:46 -0500

    run umap on data

--- checkpoint_final.py
+++ checkpoint_final.py
@@ -14,6 +14,27 @@ from helpers.context import (
 import logging as _logging
 _logger = _logging.getLogger(f'{APP_LOGGER_NAME}.CP2')
 from RankingList.dataset import UserListFilter, UserRankingColumn
+from AnimeList.clean import AnimeListClean
+from RankingList.contentcontent import UserContentScore
+import umap
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 def main():
-    ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME])
-    _logger.info(ulf.get_frame()[UserRankingColumn.USERNAME].unique().shape)
+    ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
+    # Create Users-Content with score as value
+    ucs=UserContentScore(ulf)
+    del ulf
+    # Perform UMAP
+    umap_results = ucs.run_umap()
+    f,ax = plt.subplots()
+	sns.scatterplot(ax=ax,
+		x=0, y=1,
+		hue=umap_results[1]
+		data=umap_results, legend='auto',alpha=0.5,
+		palette=sns.color_palette("mako", as_cmap=True)
+	)
+	f.set_size_inches(10,10)
+	f.set_dpi(500)
+	f.savefig('usercontent_umap_noargs.tiff')
+    # Attempt to make a user-user similarity measure by finding users with similar ratings.

95df4bc5f77efc38bcf053e4e6a487e232c761ad
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-30 11:35:11 -0500

    refactor our umap generation to require less code changes for different plots

--- checkpoint_final.py
+++ checkpoint_final.py
@@ -18,54 +18,48 @@ from AnimeList.clean import AnimeListClean
 from RankingList.contentcontent import UserContentScore
 import umap
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import Optional,Tuple,List
 from pathlib import Path
 
-def run_umap(folder:Optional[Path],data:UserContentScore, n_neighbors:int, min_dist:float,metric:str, label:str):
-	if folder is not None:
-		file = folder.joinpath(f'umap ucs {metric} {n_neighbors}_neighbors {min_dist}_dst.tiff')
-	else:
-		file=Path(f'umap ucs {metric} {n_neighbors}_neighbors {min_dist}_dst.tiff')
-	_logger.info(f'does file: {file} exist?')
-	if file.exists():
-		_logger.info('file already exists... skip.')
-		return
-	_logger.info('run umap')
-	umap_results = data.run_umap(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric, label=label)
-	_logger.info('plot data')
-	f,ax = plt.subplots()
-	sns.scatterplot(ax=ax,
-		x='UMAP-X', y='UMAP-Y', hue='UMAP-Y',
-		data=umap_results, legend='auto',alpha=0.5,
-		palette=sns.color_palette("mako", as_cmap=True)
-	)
-	f.set_size_inches(10,10)
-	f.set_dpi(500)
-	f.savefig(file)
-	plt.close(fig=f)
-
 import gc
 
-def generate_data(dataset:UserContentScore, neighbors:List, metrics:List, dist:List, folder:Path)->bool:
-	''' Returns True if interrupted. '''
+def mass_analysis(score_threshold:int, drop_below_threshold:bool,
+				  frac:float, root_folder:Path, binary:bool,
+				  neighbors:List, metrics:List, dist:List):
+	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
+	ucs=UserContentScore(
+		filter=ulf,
+		binary=binary,
+		score_threshold_gt=score_threshold,
+		frac=frac,
+		drop_below_threshold=drop_below_threshold,
+		parent_folder=root_folder
+	)
+	del ulf
+	gc.collect()
 	
 	could_not_complete:List[Tuple[Tuple,BaseException]] = []
 	interrupted:bool = False
 	for m in metrics:
-		if interrupted: break
-
+		if interrupted:
+			break
 		for n in neighbors:
-			if interrupted: break
-
+			if interrupted:
+				break
 			for d in dist:
-				if interrupted: break
-				if d is None: d = 0.0
+				if interrupted:
+					break
+				if d is None:
+					d = 0.0 # default to 0
 
 				_logger.info(f'starting umap with dist:{dist} n:{n} metric:{m}')
 				try:
-					run_umap(folder=folder,data=dataset, n_neighbors=n, min_dist=d, metric=m, label=f'{m} n{n} d{d}')
+					ucs.run_umap(
+						n_neighbors=n,
+						min_dist=d,
+						metric=m
+					)
 				except KeyboardInterrupt:
 					_logger.info('keyboard interrupt. attempting to quit.')
 					interrupted=True
@@ -79,33 +73,31 @@ def generate_data(dataset:UserContentScore, neighbors:List, metrics:List, dist:L
 	for rec in could_not_complete:
 		_logger.error(f'dist:{rec[0][0]} neigh:{rec[0][1]} metrc:{rec[0][2]}.. Failed {str(rec[1])}',exc_info=rec[1])
 	return interrupted
-
-def non_binary_analysis(folder:Path,score_threshold:int):
-	_logger.info(f'non-binary analysis. folder:{folder}, score_thresh:{score_threshold}')
-	folder.mkdir(mode=0o775, parents=False, exist_ok=True)
-	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
-	ucs=UserContentScore(filter=ulf, binary=False, score_threshold_gt=score_threshold) # Positive values marked as such.
-	del ulf
-	gc.collect()
-	metrics=['cosine','chebyshev','manhattan','euclidean']
-	neighbors = [16,32,256,1024]
-	generate_data(dataset=ucs, metrics=metrics, neighbors=neighbors, folder=folder, dist=[None])
-	pass
-
-def binary_analysis(folder:Path, score_threshold:int):
-	_logger.info(f'binary analysis. folder:{folder}, score_thresh:{score_threshold}')
-	folder.mkdir(mode=0o775, parents=False, exist_ok=True)
-	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
-	ucs=UserContentScore(filter=ulf, binary=True, score_threshold_gt=score_threshold) # Positive values marked as such.
-	del ulf
-	gc.collect()
-	metrics = ['hamming','jaccard','yule','kulsinski']
-	neighbors = [16,32,256,1024]
-	generate_data(dataset=ucs, metrics=metrics, neighbors=neighbors, folder=folder, dist=[None])
-	pass
+	
 
 def main():
-	if binary_analysis(Path('umap_binary_data_thresh5'), 5):
+	folder =Path('18 UMAP Exploration')
+
+	folder.mkdir(mode=0o775, exist_ok=True, parents=True)
+
+	neighbors = [16,32,256,1024]
+	frac_data = 0.1
+	thresh = 5
+	_logger.info('perform binary analysis')
+	if mass_analysis( # BINARY no-drop @5
+			score_threshold=thresh, frac=frac_data,
+			drop_below_threshold=False, root_folder=folder,
+			binary=True,
+			neighbors=neighbors,
+			metrics=['hamming','jaccard','yule','kulsinski'],
+			dist=[None]):
 		return
-	if non_binary_analysis(Path('umap_nonbinary_data_thresh5'), 5):
+	_logger.info('perform non-binary analysis')
+	if mass_analysis( # NON-BINARY no-drop @5
+			score_threshold=thresh, frac=frac_data,
+			drop_below_threshold=False, root_folder=folder,
+			binary=False,
+			neighbors=neighbors,
+			metrics=['cosine','chebyshev','manhattan','euclidean'],
+			dist=[None]):
 		return

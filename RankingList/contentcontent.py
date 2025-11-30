329d53e2d08e2bc8bb53285e9ec07be42acdbe1b
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-11-30 10:35:20 -0500

    methods to run binary and nonbinary umap on dataset

--- RankingList/contentcontent.py
+++ RankingList/contentcontent.py
@@ -32,7 +32,7 @@ from RankingList.dataset import (
 from AnimeList.clean import AnimeListClean
 from AnimeList.dataset import AnimeListColumns
 import logging as _logging
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, save_npz, load_npz
 _logger = _logging.getLogger(f'{APP_LOGGER_NAME}.RankingList.ContentContent')
 import umap
 
@@ -41,28 +41,61 @@ class UserContentScore():
 	matrix:csr_matrix
 	username_codes:pd.Categorical
 	item_codes:pd.Categorical
+	binary:bool
+	score_threshold:int
 	default_path:Final[Path] = Path('content-by-content.csv')
-	def __init__(self, filter:UserListFilter) -> None:
-		frame = filter.get_frame()[[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE]].dropna().copy()
-		sw = Stopwatch()
-		self.username_codes = pd.Categorical(frame[UserRankingColumn.USERNAME])
-		self.item_codes = pd.Categorical(frame[UserRankingColumn.ANIME_ID])
-		sw.start()
-		self.matrix = csr_matrix(
-			(frame[UserRankingColumn.SCORE],
-				(self.username_codes.codes, self.item_codes.codes)
-			),
-			shape=(len(self.username_codes.categories), len(self.item_codes.codes))
-		)
-		sw.end()
-		_logger.info(f'Generating content collaboration matrix took {str(sw)}')
+	base_filename:Path
+	def __init__(self, filter:UserListFilter, binary:bool, score_threshold_gt:int) -> None:
+		self.binary = binary
+		self.score_threshold = score_threshold_gt
+		self.base_filename = Path(f'UCS st_{self.score_threshold} {'binary' if self.binary else 'non-binary'}') # TYPO! should have been UCS
+		if self.base_filename.exists():
+			_logger.info('Loading UCS from file')
+			self.matrix = load_npz(self.base_filename)
+		else:
+			filter_frame = filter.get_frame()
+			_logger.info(f'oriinal filter frame shape: {filter_frame.shape}')
+			frame = filter_frame[[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE]].dropna().sample(frac=0.1,axis='index').copy()
+			_logger.info(f'Binary? {binary}, score_filter={score_threshold_gt}')
+			series_above_fiter = (frame[UserRankingColumn.SCORE] > score_threshold_gt)
+			if binary: # Make the values true & false
+				frame['TEMP_BINARY'] = False
+				frame.loc[series_above_fiter, ['TEMP_BINARY']] = True
+				frame.drop(columns=[UserRankingColumn.SCORE], inplace=True)
+				frame.rename(columns={'TEMP_BINARY':UserRankingColumn.SCORE},inplace=True)
+				_logger.info(frame.info())
+			else: # Set all values not meeting threshold to 0 & then subtract values that did by the threshold.
+				frame.loc[~series_above_fiter, [UserRankingColumn.SCORE]]=0 # Values which did not reach threshold are 0.
+				frame.loc[series_above_fiter,[UserRankingColumn.SCORE]]-=score_threshold_gt # Shift values
+				
+			_logger.info(f'after sampling frame shape: {frame.shape}')
+			sw = Stopwatch()
+			self.username_codes = pd.Categorical(frame[UserRankingColumn.USERNAME])
+			self.item_codes = pd.Categorical(frame[UserRankingColumn.ANIME_ID])
+			sw.start()
+			self.matrix = csr_matrix(
+				(frame[UserRankingColumn.SCORE],
+					(self.username_codes.codes, self.item_codes.codes)
+				),
+				shape=(len(self.username_codes.categories), len(self.item_codes.codes))
+			)
+			sw.end()
+			_logger.info(f'Generating content collaboration matrix took {str(sw)}')
+			self.save_matrix_data()
+			frame.to_csv(f'{self.base_filename}.csv',index_label='INDEX')
 
 	def get_matrix(self):
 		return self.matrix
 	
-	def run_umap(self, **kwargs):
+	def save_matrix_data(self):
+		save_npz(file=f'{self.base_filename}.npz', matrix=self.get_matrix())
+		self.username_codes
+	
+	def run_umap(self, label:str, **kwargs):
 		reducer = umap.UMAP(**kwargs)
 		data=self.get_matrix()
 		_logger.info('Plotting UMAP...')
 		embedding = reducer.fit_transform(data)
-		return pd.DataFrame(embedding, columns=['UMAP-X','UMAP-Y'])
+		umap_data = pd.DataFrame(embedding, columns=['UMAP-X','UMAP-Y'])
+		umap_data.to_csv(f'{self.base_filename}_{label}_umap.csv')
+		return umap_data
--- checkpoint_final.py
+++ checkpoint_final.py
@@ -22,8 +22,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import Optional,Tuple,List
 from pathlib import Path
-def run_umap(folder:Optional[Path],data:UserContentScore, n_neighbors:int, min_dist:float,metric:str):
-	umap_results = data.run_umap(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric)
+
+def run_umap(folder:Optional[Path],data:UserContentScore, n_neighbors:int, min_dist:float,metric:str, label:str):
+	if folder is not None:
+		file = folder.joinpath(f'umap ucs {metric} {n_neighbors}_neighbors {min_dist}_dst.tiff')
+	else:
+		file=Path(f'umap ucs {metric} {n_neighbors}_neighbors {min_dist}_dst.tiff')
+	_logger.info(f'does file: {file} exist?')
+	if file.exists():
+		_logger.info('file already exists... skip.')
+		return
+	_logger.info('run umap')
+	umap_results = data.run_umap(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric, label=label)
+	_logger.info('plot data')
 	f,ax = plt.subplots()
 	sns.scatterplot(ax=ax,
 		x='UMAP-X', y='UMAP-Y', hue='UMAP-Y',
@@ -32,33 +43,69 @@ def run_umap(folder:Optional[Path],data:UserContentScore, n_neighbors:int, min_d
 	)
 	f.set_size_inches(10,10)
 	f.set_dpi(500)
-	if folder is not None:
-		file = folder.joinpath(f'umap ucs {metric} {n_neighbors}_neighbors {min_dist}_dst.tiff')
-	else:
-		file=f'umap ucs {metric} {n_neighbors}_neighbors {min_dist}_dst.tiff'
 	f.savefig(file)
+	plt.close(fig=f)
+
+import gc
+
+def generate_data(dataset:UserContentScore, neighbors:List, metrics:List, dist:List, folder:Path)->bool:
+	''' Returns True if interrupted. '''
 	
-def main():
-	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
-	# Create Users-Content with score as value
-	ucs=UserContentScore(ulf)
-	del ulf
-	# Perform UMAP
-	folder:Path = Path('final_umap')
-	folder.mkdir(mode=0o775, parents=False, exist_ok=True)
-	min_dists=[0,0.2,0.4,0.6,0.8,1.0]
-	neighbors=[2,4,8,16,32,64,128,256,512]
-	metrics=['cosine','minkowski','chebyshev','manhattan','euclidean']
 	could_not_complete:List[Tuple[Tuple,BaseException]] = []
-	for dist in min_dists:
+	interrupted:bool = False
+	for m in metrics:
+		if interrupted: break
+
 		for n in neighbors:
-			for metric in metrics:
-				_logger.info(f'starting umap with dist:{dist} n:{n} metric:{metric}')
+			if interrupted: break
+
+			for d in dist:
+				if interrupted: break
+				if d is None: d = 0.0
+
+				_logger.info(f'starting umap with dist:{dist} n:{n} metric:{m}')
 				try:
-					run_umap(folder=folder,data=ucs, n_neighbors=n, min_dist=dist, metric=metric)
+					run_umap(folder=folder,data=dataset, n_neighbors=n, min_dist=d, metric=m, label=f'{m} n{n} d{d}')
+				except KeyboardInterrupt:
+					_logger.info('keyboard interrupt. attempting to quit.')
+					interrupted=True
 				except BaseException as e:
-					_logger.error(f'Could not complete umap... Wait for report after we run the rest')
-					could_not_complete.append(((dist,n,metric),e))
+					_logger.error(f'Could not complete umap... Wait for report after we run the rest.\n{e}')
+					could_not_complete.append(((dist,n,m),e))
+				finally:
+					plt.close('all')
+					gc.collect()
+	# Dump errors
 	for rec in could_not_complete:
-		_logger.error(f'dist:{rec[0][0]} neigh:{rec[0][1]} metrc:{rec[0][2]}.. Failed {str(rec[1])}',exc_info=rec)
-	# Attempt to make a user-user similarity measure by finding users with similar ratings.
+		_logger.error(f'dist:{rec[0][0]} neigh:{rec[0][1]} metrc:{rec[0][2]}.. Failed {str(rec[1])}',exc_info=rec[1])
+	return interrupted
+
+def non_binary_analysis(folder:Path,score_threshold:int):
+	_logger.info(f'non-binary analysis. folder:{folder}, score_thresh:{score_threshold}')
+	folder.mkdir(mode=0o775, parents=False, exist_ok=True)
+	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
+	ucs=UserContentScore(filter=ulf, binary=False, score_threshold_gt=score_threshold) # Positive values marked as such.
+	del ulf
+	gc.collect()
+	metrics=['cosine','chebyshev','manhattan','euclidean']
+	neighbors = [16,32,256,1024]
+	generate_data(dataset=ucs, metrics=metrics, neighbors=neighbors, folder=folder, dist=[None])
+	pass
+
+def binary_analysis(folder:Path, score_threshold:int):
+	_logger.info(f'binary analysis. folder:{folder}, score_thresh:{score_threshold}')
+	folder.mkdir(mode=0o775, parents=False, exist_ok=True)
+	ulf=UserListFilter(frame=None, cols=[UserRankingColumn.USERNAME,UserRankingColumn.ANIME_ID,UserRankingColumn.SCORE])
+	ucs=UserContentScore(filter=ulf, binary=True, score_threshold_gt=score_threshold) # Positive values marked as such.
+	del ulf
+	gc.collect()
+	metrics = ['hamming','jaccard','yule','kulsinski']
+	neighbors = [16,32,256,1024]
+	generate_data(dataset=ucs, metrics=metrics, neighbors=neighbors, folder=folder, dist=[None])
+	pass
+
+def main():
+	if binary_analysis(Path('umap_binary_data_thresh5'), 5):
+		return
+	if non_binary_analysis(Path('umap_nonbinary_data_thresh5'), 5):
+		return
